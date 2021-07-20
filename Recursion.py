b433577a70a0b68085b6f45069faf4ae26df269a
AppDesign1 <appdesign33@gmail.com>
2021-07-20 12:45:36 +0530

    change

--- Recursion.py
+++ Recursion.py
@@ -1,8 +1,9 @@
 def foo(n):
-  if n == 0:
-    print("Done!")
-  else:
-    print("Hello!")
-    n--;
-    foo(n)
+    if n == 0:
+        print("Done!")
+    else:
+        print("Hello!")
+        n -= 1
+        foo(n)
+
 foo(10)
