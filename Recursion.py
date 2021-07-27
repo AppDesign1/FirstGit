626d0e2b319381fdcaac1afce0df67d1b1d9603b
AppDesign1 <appdesign33@gmail.com>
2021-07-27 09:49:13 +0530

    Shortened

--- Recursion.py
+++ Recursion.py
@@ -1,18 +1,10 @@
 def foo(n):
-<<<<<<< HEAD
+
   if n == 0:
     print("Done!")
   else:
     print("Hello!")
     n -= 1
     foo(n)
-=======
-    if n == 0:
-        print("Done!")
-    else:
-        print("Hello!")
-        n -= 1
-        foo(n)
 
->>>>>>> b433577a70a0b68085b6f45069faf4ae26df269a
 foo(10)
